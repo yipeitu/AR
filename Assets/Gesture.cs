e892390c5cc8ca140b3d4a8eb9b8634a6f484d7a
Yi-Pei, Tu <f2wdulm@gmail.com>
2018-12-02 22:23:51 +0100

    Gesture with accelerometer to rotate

---
+++ Assets/Gesture.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gesture : MonoBehaviour {
+    public GameObject testObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+            Debug.Log("its here");
+            //Fetch the Renderer from the GameObject
+            Renderer rend = testObject.GetComponent<Renderer>();
+
+            //Create a new Material
+            Material material = new Material(Shader.Find("Standard"))
+            {
+                color = Color.green
+            };
+
+            //Switch to new material
+            rend.material = material;
+
+        }
+    }
+}
